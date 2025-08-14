2825a3ff1a71f023beea092bbde4868500c4204a
Sébastien Raynaud <raynaud.sn@gmail.com>
2025-08-14 19:56:52 +0200

    refactor cli to typescript

--- packages/create-react-native-wireframe/index.ts
+++ packages/create-react-native-wireframe/index.ts
@@ -1,20 +1,24 @@
 #!/usr/bin/env node
 
-const fs = require("fs-extra");
-const path = require("path");
+import fs from "fs-extra";
+import path from "path";
 
-const WIREFRAME_PACKAGE_PATH = path.dirname(
+const WIREFRAME_PACKAGE_PATH: string = path.dirname(
   require.resolve("react-native-wireframe/package.json")
 );
-const SOURCE_DIRECTORY = path.join(WIREFRAME_PACKAGE_PATH, "src");
-const DESTINATION_DIRECTORY = path.resolve(process.cwd(), "wireframe");
+const SOURCE_DIRECTORY: string = path.join(WIREFRAME_PACKAGE_PATH, "src");
+const DESTINATION_DIRECTORY: string = path.resolve(process.cwd(), "wireframe");
 
-const paths = ["components", "theme", "index.ts"].map((entry) => ({
+const paths: { source: string; destination: string }[] = [
+  "components",
+  "theme",
+  "index.ts",
+].map((entry) => ({
   source: path.join(SOURCE_DIRECTORY, entry),
   destination: path.join(DESTINATION_DIRECTORY, entry),
 }));
 
-const checkDestinationPath = () => {
+const checkDestinationPath = (): void => {
   if (fs.existsSync(DESTINATION_DIRECTORY)) {
     const destinationPath = path.basename(DESTINATION_DIRECTORY);
     console.error(`Error: The '${destinationPath}' folder already exists.`);
@@ -25,20 +29,20 @@ const checkDestinationPath = () => {
   }
 };
 
-const copyFiles = () => {
+const copyFiles = (): void => {
   fs.ensureDirSync(DESTINATION_DIRECTORY);
   paths.map((path) => {
     fs.copySync(path.source, path.destination);
   });
 };
 
-const showSuccessMessage = () => {
+const showSuccessMessage = (): void => {
   console.log(
     "Success! The wireframe components have been added to your project."
   );
 };
 
-const main = () => {
+const main = (): void => {
   checkDestinationPath();
   copyFiles();
   showSuccessMessage();
